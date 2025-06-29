ccb749619e224903d5ada9ca6880fdc96dca8169
antaresrvish <yildirimyusuf758@gmail.com>
2025-06-29 18:42:59 +0300

    refactor(profile/social-links): more dynamic components

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,41 +1,26 @@
-import { FaXTwitter, FaYoutube, FaLinkedin, FaTelegram, FaFoursquare } from "react-icons/fa6";
+import * as fa6 from "react-icons/fa6";
+import { BlurFade } from "./effects/blur-fade";
 import Tooltip from "./utils/tooltip";
-import {BlurFade} from "./effects/blur-fade";
+import { ISocialLinks } from "@/types/components/social-links";
 
-export default function socialLinks({ x, youtube, linkedin, email, foursquare }: { x: string; youtube: string; linkedin: string; email: string; foursquare: string }) {
+export default function socialLinks({ data }: { data: ISocialLinks }) {
     return (
         <BlurFade delay={1250}>
-        <div className="flex mt-10">
-            <Tooltip text="Twitter/X">
-                <a href={x} className="text-gray-900">
-                    <FaXTwitter className="inline-block w-5 h-5 mr-3.5" />
-                </a>
-            </Tooltip>
-
-            <Tooltip text="YouTube">
-                <a href={youtube} className="text-gray-900">
-                    <FaYoutube className="inline-block w-5 h-5 mr-3.5" />
-                </a>
-            </Tooltip>
-
-            <Tooltip text="LinkedIn">
-                <a href={linkedin} className="text-gray-900">
-                    <FaLinkedin className="inline-block w-5 h-5 mr-3.5"/>
-                </a>
-            </Tooltip>
-
-            <Tooltip text="Framer Marketplace">
-                <a href={foursquare} className="text-gray-900">
-                    <FaFoursquare className="inline-block w-5 h-5 mr-3.5" />
-                </a>
-            </Tooltip>
-
-            <Tooltip text="Send email">
-                <a href={email} className="text-gray-900">
-                    <FaTelegram className="inline-block w-5 h-5 mr-3.5" />
-                </a>
-            </Tooltip>
-        </div>
+            <div className="flex mt-8">
+                {data.map((item) => {
+                    const iconName = ("Fa" + item.icon) as keyof typeof fa6;
+                    const IconComponent = fa6[iconName] ? fa6[iconName] : fa6.FaQuestion;
+                    return (
+                        <Tooltip key={item.tooltip} text={item.tooltip}>
+                            <a href={item.link} className="text-gray-900">
+                                {IconComponent && (
+                                    <IconComponent className="inline-block w-5 h-5 mr-3.5" />
+                                )}
+                            </a>
+                        </Tooltip>
+                    )
+                })}
+            </div>
         </BlurFade>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,15 +15,23 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} flex justify-center h-screen bg-gray-100`}
-    >
-      <div className="flex-column w-4/6">
-        <div className="w-full mt-26 justify-start p-4">
-          <Profiler title="Hey, I'm Yusuf Yıldırım." bio1="I'm a Web Designer, Music Artist & Photographer." bio2="I spend most of the time thinking about Tea." photoUrl="/profile-photo.jpeg" />
-          <SocialLinks x="#" youtube="#" linkedin="#" email="#" foursquare="#" />
+      className={`${geistSans.className} ${geistMono.className} flex justify-center h-screen bg-gray-100 px-10`}>
+      <div className="flex-column md:w-4/5 w-full md:mt-25 mt-20">
+        <div className="w-full justify-start">
+          <Profiler profile={{
+            title: "Hey, I'm Yusuf Yıldırım.",
+            biography: [
+              "I'm a Backend Developer and Electronics Hobbyist.",
+              "I spend most of the time thinking about Tea."
+            ],
+            photoUrl: "/profile-photo.jpeg"}}/>
+          <SocialLinks data={[
+            {tooltip: "Github Profile", link: "https://www.github.com/antaresrvish", icon: "Github"},
+            {tooltip: "LinkedIn Profile", link: "https://www.linkedin.com/in/antaresrvish", icon: "Linkedin"},
+            {tooltip: "Youtube Profile", link: "https://www.youtube.com/u/antaresrvish", icon: "Youtube"}
+          ]}/>
         </div>
       </div>
-
     </div>
   );
 }
